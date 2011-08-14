4c21a4d3aea8a37639e8d51fe1cc3a4f79e0ffe1
TR Jordan <terral.jordan@gmail.com>
2011-08-14 12:57:49 -0400

    [webapp] More style tweaks.

--- hotog/places/static/js/main.js
+++ hotog/places/static/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $.get('/suggestions', {}, function(response) {
         $('#placesList').tmpl({ places: response.data }).appendTo('body');
         $('div.other-recs a.overflow').click(function() {
-            $('.overflow').show();
+            $('.overflow').animate({ 'opacity': 'toggle'}, 100);
             $(this).hide();
             return false;
         });
