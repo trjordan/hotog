dd63d9b6dd46b467ecc3061f18b379dfda163e39
TR Jordan <terral.jordan@gmail.com>
2011-08-14 12:38:14 -0400

    [settings] Updated admin contact info.

--- hotog/settings.py
+++ hotog/settings.py
@@ -12,7 +12,7 @@ DJANGO_ROOT = os.path.dirname(os.path.realpath(django.__file__))
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('TR Jordan', 'terral.jordan@gmail.com'),
 )
 
 MANAGERS = ADMINS
