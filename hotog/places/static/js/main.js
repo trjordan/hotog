ba5831a64e323ca550a452dfe984d9bb3031c3f1
TR Jordan <terral.jordan@gmail.com>
2011-08-18 23:11:34 -0400

    [webapp] Added rounding of days to to the nearest day in display.

--- hotog/places/static/js/main.js
+++ hotog/places/static/js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
     $.get('/suggestions', {}, function(response) {
-        $('#placesList').tmpl({ places: response.data }).appendTo('body');
+        var data = response.data.map(function(v) {
+            return { current: Math.round(v.current),
+                     mean: Math.round(v.mean),
+                     name: v.name };
+        });
+        $('#placesList').tmpl({ places: data }).appendTo('body');
         $('div.other-recs a.overflow').click(function() {
             $('.overflow').animate({ 'opacity': 'toggle'}, 100);
             $(this).hide();
